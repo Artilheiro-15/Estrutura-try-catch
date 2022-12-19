6ce3f92899ddc08f25717387b008490bc8beb098
Atila jr <atilla.junior33@gmail.com>
2022-12-19 16:01:11 -0300

    Segundo commit

--- Estrutura try-catch/src/App.java
+++ Estrutura try-catch/src/App.java
@@ -6,6 +6,9 @@ public class App {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    // Nesse programa eu criei um vetor de strings li uma posiçao e depois mostra a posisao isso pode
+    //gerar 2 tipos de exceção A EXCEÇAO QUE ACESEI DO ARRAY FORA DO LIMITE ou A EXCEÇAO InputMismatchException
+    // quando digita um valor invalido para o inteiro
     try {
       String[] vect = sc.nextLine().split(" ");
       int position = sc.nextInt();
